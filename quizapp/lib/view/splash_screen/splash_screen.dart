07597ad2206c7f1f6d05af4f63d71e5e1fc96f50
Prasanth3004 <vishnuprasanth3004@gmail.com>
2025-02-02 12:14:29 +0530

    splash screen

--- quizapp/lib/view/splash_screen/splash_screen.dart
+++ quizapp/lib/view/splash_screen/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:quizapp/view/home_screen/home_screen.dart'
+;import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +13,18 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen())
+      );
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:Lottie.asset('assets/animation/Animation.json'),
+    );
   }
 }
